b37225dd62549edf6b64d2224e63572fff49d416
Kevin Zhang Qing <qing.zhang@ericsson.com>
2016-11-16 09:25:39 +0800

    custom validate

--- app/main/forms.py
+++ app/main/forms.py
@@ -24,10 +24,12 @@ class CommentForm(Form):
 
 class CommentFormV(Form):
     name = StringField('', validators=[Length(0, 64)], render_kw={"placeholder": "your name"})
-
-    email = StringField('', description='* We\'ll never share your email with anyone else.', validators= \
+    email = StringField('', description='* We\'ll never share your email with anyone else', validators= \
         [DataRequired(), Length(4, 64), Email(message=u"邮件格式有误")], render_kw={"placeholder": "you@example.com"})
-    comment = TextAreaField('', description=u"* 请提出宝贵意见和建议", validators=[DataRequired()],
+    password = PasswordField('', validators=[Length(4, 8)], description='* password is 4~8 long, [A-Za-z0-9_]',
+    					render_kw={"placeholder": "password"})
+    passwordrepeat = PasswordField('', validators=[Length(4, 8)], render_kw={"placeholder": "repeat password"})
+    comment = TextAreaField('', description=u"* Comments. 请提出宝贵意见和建议", validators=[DataRequired()],
                             render_kw = {"placeholder": "input your comments here, 3~10 chars"})
     submit = SubmitField(u'Submit Comments', render_kw = {})
 
